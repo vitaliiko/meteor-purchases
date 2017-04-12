38f84106b4d1a5940acc6e331f4e27e4b1b8333f
VitaliiKobrin-in6k <vitaliik@softwareplanet.uk.com>
2017-04-12 14:55:00 +0300

    Create custom form for todos

--- client/todos/Todos.js
+++ client/todos/Todos.js
@@ -5,3 +5,18 @@ Template.Todos.helpers({
         return Todos.find({});
     }
 });
+
+Template.Todos.events({
+    'submit #new-todo'(event, template) {
+        event.preventDefault();
+        var target = event.target;
+        var name = target.name.value;
+        var whatTodo = target.whatTodo.value;
+
+        Todos.insert({
+           name, whatTodo
+        });
+
+        template.find('#new-todo').reset();
+    }
+});
