86fd19221efe9851e05fb6de4eeb6cca17e62848
VitaliiKobrin-in6k <vitaliik@softwareplanet.uk.com>
2017-04-18 13:46:37 +0300

    Add separate page for video player

--- client/video/Video.js
+++ client/video/Video.js
@@ -1,13 +1,1 @@
-Meteor.subscribe('activeTodo');
 
-Template.Video.helpers({
-    activeTodo: () => {
-        return Todos.findOne({isActive: true});
-    }
-});
-
-Template.Video.events({
-    'click .hide': function() {
-        Meteor.call('todos.deactivate', {}, () => {});
-    }
-});
